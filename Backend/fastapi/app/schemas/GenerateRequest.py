9d7542ae2540420ea66d4ca98d5afdfe9dfdd1a1
BurnKnuckle <144532387+Bookinheaven@users.noreply.github.com>
2026-07-23 18:17:21 +0530

    feat: initialize monorepo structure
    Deleted Old code and initialized mono repo structure.

--- Backend/fastapi/app/schemas/GenerateRequest.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import List
-
-class GenerateRequest(BaseModel):
-    letters: List[str] = Field(default_factory=list)
-    words: List[str] = Field(default_factory=list)
-    length: int = 20
--- Frontend/src/components/CapLock.jsx
+++
@@ -1,9 +0,0 @@
-import "../assets/svgs/lock.svg"
-
-export default function CapsLock() {
-    return (
-        <div>
-
-        </div>
-    )   
-}
